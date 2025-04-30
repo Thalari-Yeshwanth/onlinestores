a2627e10fa38be977bde73eeda7559057b0ba579
ThalariYeshwanth <yeshwanth.t@techurate.com>
2025-04-30 11:45:04 +0200

    test2

--- Frondend/onlinestore/src/MainContent.js
+++ Frondend/onlinestore/src/MainContent.js
@@ -66,7 +66,7 @@ const MainContent = ({ handleAddToCart, searchText, searchTrigger }) => {
     <div className="main-content">
       {!showAllBooks && (
         <button className="see-all-button" onClick={handleSeeAllClick}>
-          See all
+          watch all
         </button>
       )}
 
